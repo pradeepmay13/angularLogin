2eb64e082aeec68fd51d37e1c5fc26dfd775b3d2
Pradeep Verma <pradeep.verma-extern@dbschenker.com>
2017-11-14 16:54:24 +0530

    changes in angular

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,8 @@ import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angula
 })
 export class LoginComponent implements OnInit {
 	userForm: FormGroup;
-	constructor(private fb: FormBuilder, private authService:AuthService) { }
+	responseData:any;
+	constructor(private fb: FormBuilder, private authService:AuthService, private router: Router) { }
 
 	ngOnInit() {
 		this.userForm = this.fb.group({
@@ -18,14 +20,28 @@ export class LoginComponent implements OnInit {
 		})
 	}
 	formSubmit(){
-		//console.log(this.userForm.value);
-		if(this.userForm.value){
+		let frmData=this.userForm.value;
+
+		this.authService.login(frmData)
+	    .subscribe(
+	      response => {
+	        this.responseData = response;
+	        if (response.execution === '1' ) {
+	          localStorage.setItem('userData', JSON.stringify(this.responseData));
+	          this.authService.loadUserInfo();
+	          this.authService.userDetail();
+	          this.router.navigate(['./home']);
+	        } else {
+	        }
+	      }
+	    )  
+
+		/*if(this.userForm.value){
 			localStorage.setItem("userData",JSON.stringify(this.userForm.value));
 			this.authService.loadUserInfo();
 		}
 		else{
 			localStorage.clear();
-		}
+		}*/
 	}
-
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@angular/core';
-import {Headers, Http} from '@angular/http';
-import {Observable} from 'rxjs/Observable';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observer } from 'rxjs/Observer';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/toPromise';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 declare var window: any;
 
 @Injectable()
 export class AuthService {
   loggedInUser: any;
   isUserLoggedIn: boolean = false;
-  constructor() { }
-
+  userDetailDataX:any;
+  constructor(private router: Router, private http:Http) {
+  	this.loadUserInfo();
+  	this.userDetail();
+  }
+  login(data) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    return this.http.post('http://localhost/slim/public/login', JSON.stringify(data), { headers: headers })
+    .map((response: Response) => response.json())
+    .catch((error: any) => Observable.throw(error || {message: "Server Error"}));
+  }
   loadUserInfo() {
     const user = localStorage.getItem('userData') ? localStorage.getItem('userData') + '' : '';
     if (user) {
@@ -26,7 +40,35 @@ export class AuthService {
   getUserData(){
 	let username
  	username = this.loggedInUser.userName;
- 	return username
+ 	return username;
+  }
+  userLogout(){
+  	localStorage.clear();
+  	this.isUserLoggedIn = false;
+  	this.router.navigate(['./login']);
+  }
+  getUser(token) {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    return this.http.post('http://localhost/slim/public/user', JSON.stringify(token), { headers: headers })
+      .map((response: Response) => response.json() ) 
+      .catch((error: any) => Observable.throw(error || {message: 'Server Error'}));
+  }
+  userDetail(){
+    const data = JSON.parse(localStorage.getItem('userData'));    
+    if(data!=null) {
+	  const token=data.token;
+      this.getUser(token)
+      .subscribe(
+        response=>{
+          if (response.execution === true ) {    
+            this.userDetailDataX=response.resultSet[0];  
+          }else{
+            this.userDetailDataX="";
+          }
+        }
+      )
+    }    
   }
 
 }
