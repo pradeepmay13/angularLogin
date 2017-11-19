8b251dfd912f0ae1de71c0748fcba727511fb862
Pradeep <pradeepmay13@gmail.com>
2017-11-19 22:46:15 +0530

    form save

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Http, Response, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  homeForm: FormGroup;
+  @ViewChild('fileInput') fileInput;
+  constructor(private fb: FormBuilder, private elem: ElementRef) {}
 
   ngOnInit() {
+  	this.homeForm = this.fb.group({
+  		email: [],
+  		exampleSelect1: [],
+  		exampleSelect2: [],
+  		exampleTextarea: [],
+  		fileUpload : this.fb.group({
+  			exampleInputFile: []
+  		}),
+  		optionsRadios : [],
+  		agreement : []
+  	})
   }
+  saveForm() {
+  	let frmData = this.homeForm.value;
+  	let fileBrowser = this.fileInput.nativeElement;
+    if (fileBrowser.files && fileBrowser.files[0]) {
+      const formData = new FormData();
+      formData.append("image", fileBrowser.files[0]);
+
+  	console.log(formData);
+  	}
+  }
+  
 
 }
